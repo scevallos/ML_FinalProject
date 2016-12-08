d583669d60858c3d623fc55bb53aadb6e89e31b8
Sebastian <slc02014@mymail.pomona.edu>
2016-12-08 00:15:05 -0800

    forgot something lol

--- code/ml/classifiers/EnsembleClassifier.java
+++ code/ml/classifiers/EnsembleClassifier.java
@@ -1,6 +1,7 @@
 package ml.classifiers;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import ml.data.CrossValidationSet;
 import ml.data.DataSet;
@@ -8,46 +9,54 @@ import ml.data.DataSetSplit;
 import ml.data.Example;
 import ml.utils.HashMapCounter;
 
+/**
+ * This classifier is used to implement the ensemble learning techniques:
+ * boosting and bagging. The 4 classifiers used are Decision Tree, K-NN,
+ * Perceptron, and Neural Nets.
+ * 
+ * @author SC, MM, AR, EZ
+ *
+ */
 public class EnsembleClassifier implements Classifier {
 
+	// Int constants used to ID the classifiers
 	public static final int DT = 0;
 	public static final int KNN = 1;
 	public static final int PERC = 2;
 	public static final int NN = 3;
 
-	// ArrayList holding the classifiers to be used
-	private ArrayList<Classifier> classifiers;
+	// HashMap holding the classifiers to be used
+	private HashMap<Integer, Classifier> classifiers = new HashMap<Integer, Classifier>();
 
-	ArrayList<Double> alphas = new ArrayList<Double>();
+	// Contains the "scores" of each of the classifiers
+	private HashMap<Integer, Double> alphas = new HashMap<Integer, Double>();
 
 	/**
-	 * Ensemble classifier
+	 * Ensemble classifier; just a combination of the classifiers above
 	 * 
 	 * @param classifiersToUse
 	 */
 	public EnsembleClassifier(int[] classifiersToUse) {
-
-		// Initialize the ArrayList to whatever size it needs
-		classifiers = new ArrayList<Classifier>(classifiersToUse.length);
 
 		// Loop through the input array, seeing which classifiers to use
 		for (int i = 0; i < classifiersToUse.length; i++) {
 			switch (classifiersToUse[i]) {
 			case 0:
 				DecisionTreeClassifier dt = new DecisionTreeClassifier();
-				classifiers.add(dt);
+				dt.setDepthLimit(1);
+				classifiers.put(0, dt);
 				break;
 			case 1:
 				KNNClassifier knn = new KNNClassifier();
-				classifiers.add(knn);
+				classifiers.put(1, knn);
 				break;
 			case 2:
 				AveragePerceptronClassifier perc = new AveragePerceptronClassifier();
-				classifiers.add(perc);
+				classifiers.put(2, perc);
 				break;
 			case 3:
 				TwoLayerNN nn = new TwoLayerNN(2);
-				classifiers.add(nn);
+				classifiers.put(3, nn);
 				break;
 			}
 		}
@@ -62,23 +71,25 @@ public class EnsembleClassifier implements Classifier {
 	public void train(DataSet data) {
 		// Gets the new data sets via bagging method
 		ArrayList<DataSet> sets = data.getNewSets(classifiers.size());
-
+		int a = 0;
 		// Trains each classifier on its respective data set
-		for (int i = 0; i < classifiers.size(); i++)
-			classifiers.get(i).train(sets.get(i));
-
+		for (Integer i : classifiers.keySet()) {
+			classifiers.get(i).train(sets.get(a));
+			a++;
+		}
 	}
 
 	public double classify(Example example) {
 		// Used to keep count of
 		HashMapCounter<Double> predCount = new HashMapCounter<Double>();
 
-		for (Classifier c : classifiers)
-			predCount.increment(c.classify(example));
+		for (Integer i : classifiers.keySet())
+			predCount.increment(classifiers.get(i).classify(example));
 
 		// Sort them from most to least occurrences, and get the most occurring
 		// TODO: If evenly split, randomly get the first or get most confident?
-		return predCount.sortedEntrySet().get(0).getKey();
+		double pred = predCount.sortedEntrySet().get(0).getKey();
+		return pred;
 	}
 
 	/**
@@ -86,9 +97,11 @@ public class EnsembleClassifier implements Classifier {
 	 */
 	public void adaTrain(DataSet data) {
 		// sets all the weights to 1/size of data
-		setWeights(data, 1 / data.getData().size());
+		setWeights(data, 1.0 / data.getData().size());
 
-		for (Classifier c : classifiers) {
+		for (Integer i : classifiers.keySet()) {
+			Classifier c = classifiers.get(i);
+
 			c.train(data);
 
 			double alpha = calculateAlpha(data, c);
@@ -97,7 +110,7 @@ public class EnsembleClassifier implements Classifier {
 			for (Example e : data.getData())
 				Z += e.getWeight()
 						* Math.exp(-alpha * e.getLabel() * c.classify(e));
-			alphas.add(alpha);
+			alphas.put(i, alpha);
 
 			for (Example e : data.getData()) {
 
@@ -111,15 +124,13 @@ public class EnsembleClassifier implements Classifier {
 	}
 
 	public double adaClassify(Example e) {
-		int count = 0;
-		long sum = 0;
-		for (Double a : alphas) {
-			System.out.println("sum: " + sum);
-			sum += a * classifiers.get(count).classify(e);
-			count++;
-		}
+		double sum = 0;
 
-		return Math.signum(sum);
+		for (Integer i : classifiers.keySet())
+			sum += alphas.get(i) * classifiers.get(i).classify(e);
+
+		double pred = Math.signum(sum);
+		return pred;
 	}
 
 	@Override
@@ -143,58 +154,168 @@ public class EnsembleClassifier implements Classifier {
 			if (classify != e.getLabel())
 				epsilon += e.getWeight();
 		}
+		double alpha = (.5 * Math.log((1 - epsilon) / epsilon));
 
-		return (.5 * Math.log((1 - epsilon) / epsilon));
+		return alpha;
 	}
 
 	public static void main(String[] args) {
 		String path = "/home/scevallos/Documents/ML/finalProject/data/titanic-train.perc.csv";
+//		String path = "/home/scevallos/Documents/ML/finalProject/data/heart.csv";
 		DataSet data = new DataSet(path, DataSet.CSVFILE);
 
-		int[] classifiers = { EnsembleClassifier.KNN, EnsembleClassifier.PERC,
-				EnsembleClassifier.NN, EnsembleClassifier.DT };
-		EnsembleClassifier ec = new EnsembleClassifier(classifiers);
+		ArrayList<int[]> a = new ArrayList<int[]>();
+		int[] four = { EnsembleClassifier.DT, EnsembleClassifier.NN,
+				EnsembleClassifier.KNN, EnsembleClassifier.PERC };
+		// int[] three1 = { EnsembleClassifier.DT, EnsembleClassifier.NN,
+		// EnsembleClassifier.KNN };
+		// int[] three2 = { EnsembleClassifier.DT, EnsembleClassifier.NN,
+		// EnsembleClassifier.PERC };
+		// int[] three3 = { EnsembleClassifier.DT, EnsembleClassifier.KNN,
+		// EnsembleClassifier.PERC };
+		// int[] three4 = { EnsembleClassifier.NN, EnsembleClassifier.KNN,
+		// EnsembleClassifier.PERC };
+		// int[] two1 = { EnsembleClassifier.NN, EnsembleClassifier.KNN };
+		// int[] two12 = { EnsembleClassifier.KNN, EnsembleClassifier.NN };
+		// int[] two2 = { EnsembleClassifier.NN, EnsembleClassifier.PERC };
+		// int[] two3 = { EnsembleClassifier.NN, EnsembleClassifier.DT };
+		// int[] two4 = { EnsembleClassifier.KNN, EnsembleClassifier.DT };
+		// int[] two5 = { EnsembleClassifier.KNN, EnsembleClassifier.PERC };
+		// int[] two6 = { EnsembleClassifier.PERC, EnsembleClassifier.DT };
+//		 int[] knn = { EnsembleClassifier.KNN };
+//		 int[] nn = { EnsembleClassifier.NN };
+//		 int[] dt = { EnsembleClassifier.DT };
+//		 int[] perc = { EnsembleClassifier.PERC };
 
-		// THIS IS JUST SPLITTING THE DATA once and classifying. Pretty good:
-		// ~75%
-		// DataSetSplit split = data.split(0.8);
-		//
-		// System.out.println("Training...");
-		// ec.adaTrain(split.getTrain());
-		// System.out.println("Done training!");
-		//
-		// System.out.println("Classifying...");
-		// double correct = 0.0;
-		// for (Example e : split.getTest().getData()) {
-		// double pred = ec.adaClassify(e);
-		// // System.out.println("pred: " + pred);
-		// // System.out.println("label: " + e.getLabel());
-		// if (pred == e.getLabel())
-		// correct++;
-		// }
-		// double percent = correct / split.getTest().getData().size();
-		// System.out.println("Percent Correct: " + percent);
+		a.add(four);
+		// a.add(three1);
+		// a.add(three2);
+		// a.add(three3);
+		// a.add(three4);
+		// a.add(two1);
+		// a.add(two12);
+		// a.add(two2);
+		// a.add(two3);
+		// a.add(two4);
+		// a.add(two5);
+		// a.add(two6);
+//		 a.add(knn);
+//		 a.add(nn);
+//		 a.add(dt);
+//		 a.add(perc);
 
-		// CROSS VALIDATION SET TESTING: Averages a 70.6%
-		CrossValidationSet cv = data.getCrossValidationSet(10);
-		for (int i = 0; i < 10; i++) {
-			System.out.println("Split Num: " + i);
-			DataSetSplit split = cv.getValidationSet(i);
+//		int[] stumps = { EnsembleClassifier.DT, EnsembleClassifier.DT,
+//				EnsembleClassifier.DT, EnsembleClassifier.DT };
+//		a.add(stumps);
 
-			System.out.println("Training..");
-			ec.train(split.getTrain());
-			System.out.println("Classifying");
-			double correct = 0.0;
-			for (Example e : split.getTest().getData()) {
-				double pred = ec.classify(e);
-				if (pred == e.getLabel())
-					correct++;
+		for (int k = 0; k < a.size(); k++) {
+			EnsembleClassifier ec = new EnsembleClassifier(a.get(k));
+			for (int j = 0; j < a.get(k).length; j++)
+				System.out.println("array is: " + a.get(k)[j]);
+
+			// THIS IS JUST SPLITTING THE DATA once and classifying. Pretty
+			// good:
+			// ~75%
+			// DataSetSplit split = data.split(0.8);
+			//
+			// System.out.println("Training...");
+			// ec.adaTrain(split.getTrain());
+			// System.out.println("Done training!");
+			//
+			// System.out.println("Classifying...");
+			// double correct = 0.0;
+			// for (Example e : split.getTest().getData()) {
+			// double pred = ec.adaClassify(e);
+			// // System.out.println("pred: " + pred);
+			// // System.out.println("label: " + e.getLabel());
+			// if (pred == e.getLabel())
+			// correct++;
+			// }
+			// double percent = correct / split.getTest().getData().size();
+			// System.out.println("Percent Correct: " + percent);
+
+			// CROSS VALIDATION SET TESTING: Averages a 70.6%
+			CrossValidationSet cv = new CrossValidationSet(data, 10, true);
+
+			ArrayList<Double> percents = new ArrayList<Double>();
+			long trainSum = 0;
+			long classifySum = 0;
+			for (int i = 0; i < 10; i++) {
+				// System.out.println("Split Num: " + i);
+				DataSetSplit split = cv.getValidationSet(i);
+
+				// System.out.println("Training..");
+				System.gc();
+				long start = System.currentTimeMillis();
+				ec.train(split.getTrain());
+				trainSum += System.currentTimeMillis() - start;
+
+				// System.out.println("Classifying");
+				System.gc();
+				start = System.currentTimeMillis();
+				double correct = 0.0;
+				for (Example e : split.getTest().getData()) {
+					double pred = ec.classify(e);
+					if (pred == e.getLabel())
+						correct++;
+				}
+				classifySum += System.currentTimeMillis() - start;
+
+				double percent = correct / split.getTest().getData().size();
+				percents.add(percent);
+				System.out.println("percent correct: " + percent);
 			}
+			// System.out.println("Average train time: " + ((double) trainSum)
+			// / 10 / 1000 + "s");
+			// System.out.println("Average test time: " + ((double) classifySum)
+			// / 10 / 1000 + "s");
 
-			double percent = correct / split.getTest().getData().size();
-			System.out.println("percent correct: " + percent);
+			double sum = 0.0;
+			for (double p : percents)
+				sum += p;
+
+			double average = sum / 10.0;
+			System.out.println("Average is: " + average);
 		}
-
+		
+		
+//		// Testing classifiers alone
+//		KNNClassifier KNN = new KNNClassifier();
+//		TwoLayerNN NN = new TwoLayerNN(2);
+//		DecisionTreeClassifier DT = new DecisionTreeClassifier();
+//		PerceptronClassifier PERC = new PerceptronClassifier();
+//		
+//		CrossValidationSet cv = data.getCrossValidationSet(10);
+//		ArrayList<HashMap<String, Double>> percs = new ArrayList<HashMap<String, Double>>(10);
+//		for(int c = 0; c < 10; c++){
+//			DataSetSplit split = cv.getValidationSet(c);
+//			
+//			KNN.train(split.getTrain());
+//			NN.train(split.getTrain());
+//			DT.train(split.getTrain());
+//			PERC.train(split.getTrain());
+//			
+//			double[] preds = {0.0, 0.0, 0.0, 0.0};
+//			double[] cors = {0.0, 0.0, 0.0, 0.0};
+//			for (Example e: split.getTest().getData()){
+//				preds[0] = KNN.classify(e);
+//				preds[1] = NN.classify(e);
+//				preds[2] = DT.classify(e);
+//				preds[3] = PERC.classify(e);
+//				for(int j = 0; j < 4; j++){
+//					if (preds[j] == e.getLabel())
+//						cors[j]++;
+//				}
+//			}
+//			
+//			HashMap<String, Double> percents = new HashMap<String, Double>();
+//			percents.put("KNN", cors[0]/split.getTest().getData().size());
+//			percents.put("NN", cors[1]/split.getTest().getData().size());
+//			percents.put("DT", cors[2]/split.getTest().getData().size());
+//			percents.put("PERC", cors[3]/split.getTest().getData().size());
+//			percs.add(percents);
+//		}
+//		System.out.println("percents are: " + percs);
+//		System.out.println("done! :)");
 	}
-
 }
